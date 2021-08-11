d3c0c6d03c948f5c54c7000e4a096b6fd3cc0e4b
faridun96 <faridun.nk@gmail.com>
2021-08-11 10:40:02 +0500

    Discount

---
+++ DiscountApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DiscountApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вычисление стоимости покупки с учетом скидки.");
+            Console.Write("Введите сумму покупки: ");
+            double summa = int.Parse(Console.ReadLine());
+            if (summa>500 && summa<1000)
+            {
+                summa=summa*(1 - 0.03);
+                Console.WriteLine("Вам предоставлена скидка в размере 3%");
+                Console.WriteLine($"Сумма покупки с учетом скидки: {Math.Round(summa,2)}");
+            }
+            else if (summa>1000) 
+            {
+                summa=summa*(1 - 0.05);
+                Console.WriteLine("Вам предоставлена скидка в размере 5%");
+                Console.WriteLine($"Сумма покупки с учетом скидки: {Math.Round(summa,2)}");
+            }
+            else
+            {
+                Console.WriteLine("Скидка не предоставляется");
+            }
+        }
+    }
+}
